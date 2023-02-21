e45b6d8f11ccdee6926a1b3db32fdd62f96d3b7b
Dan Stronge <dnlstronge@gmail.com>
2023-02-21 08:35:44 +0000

    [x] move logic into separate provider component

--- src/components/context/authContext.js
+++ src/components/context/authContext.js
@@ -1,10 +1,29 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 const AuthContext = React.createContext({
     isLoggedIn: false,
-    onLogout: () => {
-
-    }
+    onLogout: () => {}
 })
+
+export const AuthContextProvider = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const logoutHandler = () => {
+        setIsLoggedIn(false)
+    }
+
+    const loginHandler = () => {
+        setIsLoggedIn(true)
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            isLoggedIn: isLoggedIn, 
+            onLogout: logoutHandler, 
+            onLogin: loginHandler  }}>
+
+        </AuthContext.Provider>
+    )
+}
 
 export default AuthContext;
