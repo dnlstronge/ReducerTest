9e60f7d5de852f2845b452d037169c4c8b2baab7
Dan Stronge <dnlstronge@gmail.com>
2023-02-21 08:48:45 +0000

    [x] - update props etc with context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/context/authContext';
 
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-
+  const context = useContext(AuthContext)
 
   return (
     <React.Fragemnt>
-        <MainHeader isAuthenticated={isLoggedIn}  />
+        <MainHeader isAuthenticated={context.isLoggedIn}  />
         <main>
-          {!isLoggedIn && <Login />}
-          {isLoggedIn && <Home />}
+          {!context.isLoggedIn && <Login />}
+          {context.isLoggedIn && <Home />}
         </main>
     </React.Fragemnt>
   );
